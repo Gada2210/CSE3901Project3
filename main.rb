e15890266e49d42c4ad9d989371739b81affe049
Hannah Ackley <ackley.77@osu.edu>
2023-02-12 21:57:17 -0500

    Add parser to main that writes headlines to a txt file

--- main.rb
+++ main.rb
@@ -2,6 +2,27 @@ require "mechanize"
 require "nokogiri"
 
 agent = Mechanize.new
-page = agent.get "https://news.osu.edu/"
+page = agent.get("https://news.osu.edu/").body
 
-puts "hello world!"
+# content = page.read
+
+# Create a new file to write to
+fp = File.new("OSU News.txt", "w")
+
+# Create an instance of Nokogiri to search page with CSS selectors
+parsed_content = Nokogiri::HTML(page)
+
+
+# Write header to output file
+fp.write("OSU News Digest\n\n")
+
+
+# Search page using CSS selectors
+PATH_FORMAT = Regexp.new("\/\/\*\/div\[1]\/div\[1]\/div\[\d\]\/div\/div\[3]\/div\[1]\/h3")
+
+parsed_content.xpath("//h3").each do |headline|
+    fp.write(headline.content, "\n")
+end
+
+#<h3 class="pp-block-item-heading">How sharing kindness helps ease anxiety and depression</h3>
+
